a5b4887c90a2df4a0545c2f00024e147d285b686
radoncreep <radoncreep@gmail.com>
2021-07-08 23:13:42 +0100

    logic

---
+++ js/components.mjs
@@ -0,0 +1,36 @@
+let btn = document.querySelector('#create');
+let formDiv = document.querySelector('.choose');
+let input = document.createElement('input');
+
+// BUTTONS
+let submitButton = document.createElement('button');
+
+let randomBtn =  document.createElement('button');
+randomBtn.innerHTML = "Random";
+randomBtn.setAttribute('id', 'rndBtn');
+
+let emptyBtn =  document.createElement('button');
+emptyBtn.innerHTML = "Empty";
+
+let bfsButton = document.createElement('button')
+bfsButton.setAttribute('id', 'bfsBtn');
+bfsButton.innerHTML = 'BFS';
+console.log(bfsButton)
+
+let dfsButton = document.createElement('button')
+dfsButton.setAttribute('id', 'dfsBtn');;
+dfsButton.innerHTML = 'DFS';
+
+
+export {
+    bfsButton,
+    btn,
+    dfsButton,
+    emptyBtn,
+    formDiv,
+    input,
+    randomBtn,
+    submitButton,
+}
+
+
--- js/visual.mjs
+++ js/visual.mjs
@@ -1,27 +1,96 @@
 import { BinarySearchTree } from "./Tree.mjs";
 
-const tree = new BinarySearchTree();
+import { 
+    btn,
+    formDiv,
+    input,
+    randomBtn,
+    emptyBtn,
+    bfsButton,
+    dfsButton
+ } from "./components.mjs";
 
-tree.insertNode(7);
-tree.insertNode(4);
-tree.insertNode(10);
-tree.insertNode(8);
-tree.insertNode(16);
-tree.insertNode(0);
-tree.insertNode(5);
+//  GLOBAL VARIABLES
+let tree = new BinarySearchTree();
 
-const creatButton = d3.selectAll('button').on('click', inputVisible);
-const buttonContainer = d3.select('.choose');
+// STATES
+let isCreateVisible = false;
 
-let inputStyle = {
-    // width: '20px',
-    // height: '40px'
+// FUNCTIONS
+const setOpen = (value) => {
+    isCreateVisible = value;
 }
-function inputVisible() {
-    buttonContainer
-        .append('input')
+
+// UI FUNCTIONALITY
+const inputVisible = () => {
+    if (!isCreateVisible) {
+        formDiv.appendChild(randomBtn);
+        formDiv.appendChild(emptyBtn);
+        setOpen(true)
+    } else {
+        formDiv.removeChild(randomBtn);
+        formDiv.removeChild(emptyBtn);
+        setOpen(false);
+    }
 }
+
+const handleTraversalUI = () => {
+    let traversalDiv = document.querySelector('.traversal-container');
+    traversalDiv.appendChild(bfsButton);
+    traversalDiv.appendChild(dfsButton);
+}
+
+const generateRandomTree = () => {
+    let data = [];
+    tree = new BinarySearchTree();
+
+    while (data.length <= 8) {
+        let randomNum = Math.floor(Math.random() * 20) + 1;
+        if (data.indexOf(randomNum) === -1 ) data.push(randomNum);
+    };
+    console.log(data);
+
+    data.forEach((elem) => tree.insertNode(elem));
+
+    console.log(JSON.stringify(tree));
+}
+
+const generateEmptyTree = () => {
+    tree = null;
+    console.log(tree)
+}
+
+// DOM
+const submitButton = document.createElement('button');
+submitButton.innerHTML = "Submit";
+
+// EVENTS
+// submitButton.addEventListener('submit', submitFunc)
+btn.addEventListener('click', inputVisible);
+randomBtn.addEventListener('click', generateRandomTree);
+emptyBtn.addEventListener('click', generateEmptyTree);
+
+// TRAVERSAL BTNS EVENTS
+let traversalBtn = document.querySelector('#traversal');
+traversalBtn.addEventListener('click', handleTraversalUI);
+
+
+
+
+
 
 // console.log(JSON.stringify(tree.lookup(4)));
 
 // console.log(JSON.stringify(tree));
+// const creatButton = d3.selectAll('button').on('click', inputVisible);
+// const buttonContainer = d3.select('.choose');
+
+// const tree = new BinarySearchTree();
+
+// tree.insertNode(7);
+// tree.insertNode(4);
+// tree.insertNode(10);
+// tree.insertNode(8);
+// tree.insertNode(16);
+// tree.insertNode(0);
+// tree.insertNode(5);
